9745545b666a2f3c49bb23fdf708eca8877cca4d
Nikola <knez.nikola00@outlook.com>
2023-06-09 21:24:27 +0200

    remove notification controller

--- BE/src/Modules/Notification/NewAvalon.Notification.Boundary/Notifications/Commands/SendEmail/SendEmailRequest.cs
+++
@@ -1,4 +0,0 @@
-﻿namespace NewAvalon.Notification.Boundary.Notifications.Commands.SendEmail
-{
-    public sealed record SendEmailRequest(string SenderEmail, string RecieverEmail, string Message);
-}
--- BE/src/Modules/Notification/NewAvalon.Notification.Presentation/Controllers/NotificationsController.cs
+++ BE/src/Modules/Notification/NewAvalon.Notification.Presentation/Controllers/NotificationsController.cs
@@ -1,10 +1,4 @@
-﻿using Mapster;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using NewAvalon.Notification.Boundary.Notifications.Commands.SendEmail;
-using NewAvalon.Notification.Presentation.Abstractions;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using NewAvalon.Notification.Presentation.Abstractions;
 
 namespace NewAvalon.Notification.Presentation.Controllers
 {
@@ -13,24 +7,5 @@ namespace NewAvalon.Notification.Presentation.Controllers
     /// </summary>
     public sealed class NotificationsController : NotificationModuleController
     {
-        /// <summary>
-        /// Sends email.
-        /// </summary>
-        /// <param name="message">Message for tech support.</param>
-        /// <param name="cancellationToken">The cancellation token.</param>
-        /// <returns>The identifier of the newly created line item.</returns>
-        [HttpPost]
-        [ProducesResponseType(StatusCodes.Status204NoContent)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(StatusCodes.Status403Forbidden)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<IActionResult> SendEmail([FromBody] SendEmailRequest request, CancellationToken cancellationToken)
-        {
-            var command = request.Adapt<SendEmailCommand>();
-
-            await Sender.Send(command, cancellationToken);
-
-            return NoContent();
-        }
     }
 }
